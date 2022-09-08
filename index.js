46f62b30005c6839c9ea805492c574e5af1f3db9
FuzzyKittyz <topher.a@hotmail.com>
2022-09-08 14:52:48 -0600

    add flyover times

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const { cookie } = require('request');
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss')
+const { fetchISSFlyOverTimes } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+//fetchMyIP((error, ip) => {
+  //if (error) {
+    //console.log("It didn't work!" , error);
+    //return;
+  //}
 
-  console.log('It worked! Returned IP:' , ip);
-});
+  //console.log('It worked! Returned IP:' , ip);
+//});
 
 //fetchCoordsByIP('184.64.41.34', (error, data) => {
   //if (error) {
@@ -18,3 +19,14 @@ fetchMyIP((error, ip) => {
   //}
   //console.log('It worked! Returned coordinates:', data)
 //})
+
+//const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+//fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  //if (error) {
+    //console.log("It didn't work!" , error);
+    //return;
+  //}
+
+  //console.log('It worked! Returned flyover times:' , passTimes);
+//});
