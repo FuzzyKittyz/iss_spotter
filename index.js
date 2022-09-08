20494fdb04025d92e563e434561bff681d8f26e5
FuzzyKittyz <topher.a@hotmail.com>
2022-09-08 15:00:38 -0600

    finished iss_spotter

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { cookie } = require('request');
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss')
 const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
 //fetchMyIP((error, ip) => {
   //if (error) {
@@ -30,3 +31,20 @@ const { fetchISSFlyOverTimes } = require('./iss');
 
   //console.log('It worked! Returned flyover times:' , passTimes);
 //});
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
