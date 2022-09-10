5bf504c0cdd53a7a82c28c5a8e9eb4f939b9efed
FuzzyKittyz <topher.a@hotmail.com>
2022-09-10 12:12:54 -0600

    iss spotter using promises

---
+++ index2.js
@@ -0,0 +1,9 @@
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss_promised');
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
